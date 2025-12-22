226c926a315b48ebc2d5f31083845e425c41ddec
Abdelrahman <abdelrahman.t.abdelhamid@gmail.com>
2025-12-22 16:06:00 +0200

    Add alias and optimize dependencies for Embedded SDK in Vite configuration
    - Include '@salla.sa/embedded-sdk' in optimizeDeps for improved performance.
    - Set up a path alias for the Embedded SDK to streamline imports.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   root: ".",
   build: {
     outDir: "dist",
   },
+  optimizeDeps: {
+    include: ['@salla.sa/embedded-sdk']
+  },
+  resolve: {
+    alias: {
+      '@salla.sa/embedded-sdk': resolve('./node_modules/@salla.sa/embedded-sdk/dist/esm/index.js')
+    }
+  }
 });
